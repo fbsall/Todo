c28bd92a9d3c81c5eefbe53924f294576ac5de37
fbsall <fbsall@outlook.com>
2019-05-20 12:41:03 +0000

    Bugs correction

--- app.js
+++ app.js
@@ -1,37 +1,45 @@
-var require('express');
-var require('cookie-session');
-var require('body-parser');
-var urlencoderParser =  require(bodyParser.urlencoded({extended: false});
+var express= require('express');
+var session = require('cookie-session');
+var bodyParser = require('body-parser');
+var urlencoderParser =  bodyParser.urlencoded({extended:false});
 
 var app = express();
 
 //session initialisation - param secret is require
-app.use(session({secret: 'topsecret'}));
+app.use(session({secret: 'topsecret'}))
 
 //Create a session with an empty array if it doesn't exist
-.use(function(res, req, next){
+.use(function(req, res, next){
   if(typeof(req.session.todolist) == 'undefined'){
     req.session.todolist = [];
+  }
     next();
-});
+})
 
 //Display the todolist and the form
-.get('/todo', function(res, req){
+.get('/todo', function(req, res){
   res.render('todo.ejs', {todolist: req.session.todolist});
-});
+})
 
-.post('/todo/add', urlencoderParser, function(res, req){
+//Add a new item to the todo list
+.post('/todo/add/', urlencoderParser, function(req, res){
   if( req.session.newtodo != ''){
-    req.session.todolist.push(req.)
+    req.session.todolist.push(req.body.newtodo);
   }
   res.redirect('/todo')
-});
+})
 
-});
-.get('/todo/delete/:id', function(res, req){
+// Delete an item from the todo list
+.get('/todo/delete/:id', function(req, res){
   if(req.params.id != ''){
     req.session.todolist.splice(req.params.id, 1);
   }
   res.redirect('/todo');
+})
 
-});
+//return todo page for 404 page
+.use(function(req, res, next){
+  res.redirect('/todo');
+})
+
+.listen(8080);
